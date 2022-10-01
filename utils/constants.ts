e9bfc6c6b7bee1dfc943f90b48a3b91dea3b0157
manitej <pratamaniteja66@gmail.com>
2022-10-01 16:09:27 +0530

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const HOST_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_URL
+    ? "https://yt-tools.vercel.app"
     : "http://localhost:3000";
 export const chromePath =
   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
